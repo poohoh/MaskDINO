f81eaa42cd7a4e4e0f577654e1ae280cc75da9ba
poohoh <kjh980328@gmail.com>
2023-10-16 14:03:56 +0900

    get masks from image

--- demo/demo.py
+++ demo/demo.py
@@ -52,18 +52,18 @@ def get_parser():
     )
     parser.add_argument("--webcam", action="store_true", help="Take inputs from webcam.")
     parser.add_argument("--video-input", help="Path to video file.")
-    parser.add_argument(
-        "--input",
-        nargs="+",
-        help="A list of space separated input images; "
-        "or a single glob pattern such as 'directory/*.jpg'",
-    )
+    # parser.add_argument(
+    #     "--input",
+    #     nargs="+",
+    #     help="A list of space separated input images; "
+    #     "or a single glob pattern such as 'directory/*.jpg'",
+    # )
     parser.add_argument(
         "--output",
+        default="output",
         help="A file or directory to save output visualizations. "
         "If not given, will show output in an OpenCV window.",
     )
-
     parser.add_argument(
         "--confidence-threshold",
         type=float,
@@ -76,119 +76,140 @@ def get_parser():
         default=[],
         nargs=argparse.REMAINDER,
     )
+    parser.add_argument('--root_dir', default='C:/Users/KimJunha/Desktop/test/car/000001', type=str, help='image dir')
+    parser.add_argument('--bg_dir', default='C:/Users/KimJunha/Desktop/test/background', type=str)
+
     return parser
 
 
-def test_opencv_video_format(codec, file_ext):
-    with tempfile.TemporaryDirectory(prefix="video_format_test") as dir:
-        filename = os.path.join(dir, "test_file" + file_ext)
-        writer = cv2.VideoWriter(
-            filename=filename,
-            fourcc=cv2.VideoWriter_fourcc(*codec),
-            fps=float(30),
-            frameSize=(10, 10),
-            isColor=True,
-        )
-        [writer.write(np.zeros((10, 10, 3), np.uint8)) for _ in range(30)]
-        writer.release()
-        if os.path.isfile(filename):
-            return True
-        return False
+# def test_opencv_video_format(codec, file_ext):
+#     with tempfile.TemporaryDirectory(prefix="video_format_test") as dir:
+#         filename = os.path.join(dir, "test_file" + file_ext)
+#         writer = cv2.VideoWriter(
+#             filename=filename,
+#             fourcc=cv2.VideoWriter_fourcc(*codec),
+#             fps=float(30),
+#             frameSize=(10, 10),
+#             isColor=True,
+#         )
+#         [writer.write(np.zeros((10, 10, 3), np.uint8)) for _ in range(30)]
+#         writer.release()
+#         if os.path.isfile(filename):
+#             return True
+#         return False
 
 
 if __name__ == "__main__":
-    mp.set_start_method("spawn", force=True)
+    mp.set_start_method("spawn", force=True)  # spawn: 부모 프로세스가 새로운 프로세스 시작 후 자식 프로세스 호출, force: 현재 설정된 시작 방법을 무시하고 새로운 방법 강제 설정
     args = get_parser().parse_args()
-    setup_logger(name="fvcore")
-    logger = setup_logger()
-    logger.info("Arguments: " + str(args))
+    setup_logger(name="fvcore")  # logger 설정 함수, logger의 이름이 fvcore
+    logger = setup_logger()  # 기본 로거 설정
+    logger.info("Arguments: " + str(args))  # 로그 메시지를 info level로 출력. args 출력
 
     cfg = setup_cfg(args)
 
     demo = VisualizationDemo(cfg)
 
-    if args.input:
-        if len(args.input) == 1:
-            args.input = glob.glob(os.path.expanduser(args.input[0]))
-            assert args.input, "The input path(s) was not found"
-        for path in tqdm.tqdm(args.input, disable=not args.output):
-            # use PIL, to be consistent with evaluation
-            img = read_image(path, format="BGR")
-            start_time = time.time()
-            predictions, visualized_output = demo.run_on_image(img)
-            logger.info(
-                "{}: {} in {:.2f}s".format(
-                    path,
-                    "detected {} instances".format(len(predictions["instances"]))
-                    if "instances" in predictions
-                    else "finished",
-                    time.time() - start_time,
-                )
-            )
+    # read images
+    paths = glob.glob(os.path.join(args.root_dir, '**/*.png'), recursive=True)
 
-            if args.output:
-                if os.path.isdir(args.output):
-                    assert os.path.isdir(args.output), args.output
-                    out_filename = os.path.join(args.output, os.path.basename(path))
-                else:
-                    assert len(args.input) == 1, "Please specify a directory with args.output"
-                    out_filename = args.output
-                visualized_output.save(out_filename)
-            else:
-                cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-                cv2.imshow(WINDOW_NAME, visualized_output.get_image()[:, :, ::-1])
-                if cv2.waitKey(0) == 27:
-                    break  # esc to quit
-    elif args.webcam:
-        assert args.input is None, "Cannot have both --input and --webcam!"
-        assert args.output is None, "output not yet supported with --webcam!"
-        cam = cv2.VideoCapture(0)
-        for vis in tqdm.tqdm(demo.run_on_video(cam)):
-            cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-            cv2.imshow(WINDOW_NAME, vis)
-            if cv2.waitKey(1) == 27:
-                break  # esc to quit
-        cam.release()
-        cv2.destroyAllWindows()
-    elif args.video_input:
-        video = cv2.VideoCapture(args.video_input)
-        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frames_per_second = video.get(cv2.CAP_PROP_FPS)
-        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        basename = os.path.basename(args.video_input)
-        codec, file_ext = (
-            ("x264", ".mkv") if test_opencv_video_format("x264", ".mkv") else ("mp4v", ".mp4")
-        )
-        if codec == ".mp4v":
-            warnings.warn("x264 codec not available, switching to mp4v")
-        if args.output:
-            if os.path.isdir(args.output):
-                output_fname = os.path.join(args.output, basename)
-                output_fname = os.path.splitext(output_fname)[0] + file_ext
-            else:
-                output_fname = args.output
-            assert not os.path.isfile(output_fname), output_fname
-            output_file = cv2.VideoWriter(
-                filename=output_fname,
-                # some installation of opencv may not support x264 (due to its license),
-                # you can try other format (e.g. MPEG)
-                fourcc=cv2.VideoWriter_fourcc(*codec),
-                fps=float(frames_per_second),
-                frameSize=(width, height),
-                isColor=True,
-            )
-        assert os.path.isfile(args.video_input)
-        for vis_frame in tqdm.tqdm(demo.run_on_video(video), total=num_frames):
-            if args.output:
-                output_file.write(vis_frame)
-            else:
-                cv2.namedWindow(basename, cv2.WINDOW_NORMAL)
-                cv2.imshow(basename, vis_frame)
-                if cv2.waitKey(1) == 27:
-                    break  # esc to quit
-        video.release()
-        if args.output:
-            output_file.release()
-        else:
-            cv2.destroyAllWindows()
+    for path in tqdm.tqdm(paths, disable=not args.output):
+        # use PIL, to be consistent with evaluation
+        img = read_image(path, format="BGR")
+        start_time = time.time()
+        predictions, visualized_output = demo.run_on_image(img)
+
+        # biggest mask
+        masks = predictions['instances'].get('pred_masks').cpu().numpy()
+        if len(masks) < 1:
+            continue
+        areas = [np.sum(mask) for mask in masks]
+        mask_main = masks[np.argmax(areas)]
+        mask_main = (mask_main * 255).astype(np.uint8)
+
+        # cv2.imshow('mask_main', mask_main)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+
+        # save mask
+        save_dir = os.path.join(args.root_dir, 'output')
+        os.makedirs(save_dir, exist_ok=True)
+        img_name = os.path.splitext(os.path.basename(path))[0]
+        cv2.imwrite(os.path.join(save_dir, f'{img_name}_mask.png'), mask_main)
+
+        # logger.info(
+        #     "{}: {} in {:.2f}s".format(
+        #         path,
+        #         "detected {} instances".format(len(predictions["instances"]))
+        #         if "instances" in predictions
+        #         else "finished",
+        #         time.time() - start_time,
+        #     )
+        # )
+
+        # if args.output:
+        #     if os.path.isdir(args.output):
+        #         assert os.path.isdir(args.output), args.output
+        #         out_filename = os.path.join(args.output, os.path.basename(path))
+        #     else:
+        #         assert len(args.input) == 1, "Please specify a directory with args.output"
+        #         out_filename = args.output
+        #     visualized_output.save(out_filename)
+        # else:
+        #     cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+        #     cv2.imshow(WINDOW_NAME, visualized_output.get_image()[:, :, ::-1])
+        #     if cv2.waitKey(0) == 27:
+        #         break  # esc to quit
+    # elif args.webcam:
+    #     assert args.input is None, "Cannot have both --input and --webcam!"
+    #     assert args.output is None, "output not yet supported with --webcam!"
+    #     cam = cv2.VideoCapture(0)
+    #     for vis in tqdm.tqdm(demo.run_on_video(cam)):
+    #         cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+    #         cv2.imshow(WINDOW_NAME, vis)
+    #         if cv2.waitKey(1) == 27:
+    #             break  # esc to quit
+    #     cam.release()
+    #     cv2.destroyAllWindows()
+    # elif args.video_input:
+    #     video = cv2.VideoCapture(args.video_input)
+    #     width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    #     height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    #     frames_per_second = video.get(cv2.CAP_PROP_FPS)
+    #     num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    #     basename = os.path.basename(args.video_input)
+    #     codec, file_ext = (
+    #         ("x264", ".mkv") if test_opencv_video_format("x264", ".mkv") else ("mp4v", ".mp4")
+    #     )
+    #     if codec == ".mp4v":
+    #         warnings.warn("x264 codec not available, switching to mp4v")
+    #     if args.output:
+    #         if os.path.isdir(args.output):
+    #             output_fname = os.path.join(args.output, basename)
+    #             output_fname = os.path.splitext(output_fname)[0] + file_ext
+    #         else:
+    #             output_fname = args.output
+    #         assert not os.path.isfile(output_fname), output_fname
+    #         output_file = cv2.VideoWriter(
+    #             filename=output_fname,
+    #             # some installation of opencv may not support x264 (due to its license),
+    #             # you can try other format (e.g. MPEG)
+    #             fourcc=cv2.VideoWriter_fourcc(*codec),
+    #             fps=float(frames_per_second),
+    #             frameSize=(width, height),
+    #             isColor=True,
+    #         )
+    #     assert os.path.isfile(args.video_input)
+    #     for vis_frame in tqdm.tqdm(demo.run_on_video(video), total=num_frames):
+    #         if args.output:
+    #             output_file.write(vis_frame)
+    #         else:
+    #             cv2.namedWindow(basename, cv2.WINDOW_NORMAL)
+    #             cv2.imshow(basename, vis_frame)
+    #             if cv2.waitKey(1) == 27:
+    #                 break  # esc to quit
+    #     video.release()
+    #     if args.output:
+    #         output_file.release()
+    #     else:
+    #         cv2.destroyAllWindows()
